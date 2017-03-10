9191c37f65a14904a49e47e54d6a43a424bff8da
clove506 <LuJiang@youhujia.com>
2017-03-10 11:39:08 +0800

    update updat_content.sh

--- update_content.sh
+++ update_content.sh
@@ -2,5 +2,5 @@
 
 . /home/deploy/.zshrc
 
-cd /opt/swagger && git pull >> /home/deploy/cc 2>&1
-cd /opt/swagger && /home/deploy/.nvm/versions/node/v4.6.0/bin/node index.js >> /home/deploy/cc 2>&1
+cd /opt/swagger && git pull >> /home/deploy/lj 2>&1
+cd /opt/swagger && /home/deploy/.nvm/versions/node/v4.6.0/bin/node index.js >> /home/deploy/lj 2>&1
